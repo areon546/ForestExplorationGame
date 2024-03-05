5ae54dd22655ed56daf3b0aae158eee18908f216
Areon546 <57325743+areon546@users.noreply.github.com>
2024-03-05 13:08:48 +0000

    Updated the Animal class in Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,10 +1,44 @@
 
 public class Animal {
-
     String name;
     boolean nocturnal = false;
     boolean healthy = false;
     boolean aggresive = false;
     Plant herbRequired;
     String interactionText;
+
+    // initialises an Animal object
+    //
+    public Animal(String name, boolean nocturnal, boolean healthy, boolean aggresive,
+            String herbReq, String interactionText) {
+
+        this.name = name;
+        this.nocturnal = nocturnal;
+        this.healthy = healthy;
+        this.aggresive = aggresive;
+        this.herbRequired = Plant.getPlant(herbReq);
+        this.interactionText = interactionText;
+    } // END initAnimal
+
+    // gets the animal's required herb
+    //
+    public Plant getHerbRequired() {
+        return this.herbRequired;
+    } // END getAnimalHerbReq
+
+    // returns the inputted animal's name field
+    //
+    public String getName() {
+        return this.name;
+    } // END getAnimalName
+
+    // returns the animal interaction
+    //
+    public String getInteractionText() {
+        return this.interactionText;
+    } // END getAnimalInteraction
+
+    public boolean getHealthy() {
+        return this.healthy;
+    }
 }
